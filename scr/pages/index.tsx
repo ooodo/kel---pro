4a1254efd582e6e4e3539a8a8cdd54f80223bdc8
ZILLAN ✅ <gson62957@gmail.com>
2025-11-02 13:00:49 -0800

    Update index.tsx

---
+++ scr/pages/index.tsx
@@ -0,0 +1,9 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>مرحباً بك في موقع KEL</h1>
+      <p>تم إعداد المشروع بنجاح</p>
+    </div>
+  )
+}
